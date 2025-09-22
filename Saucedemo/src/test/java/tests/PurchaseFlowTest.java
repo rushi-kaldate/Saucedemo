66484fcff9a6f7041c0244d0abdbca4aa8de7f15
rushi-kaldate <rushabhkaldate@gmail.com>
2025-09-22 18:44:03 +0530

    Update PurchaseFlowTest.java

--- Saucedemo/src/test/java/tests/PurchaseFlowTest.java
+++ Saucedemo/src/test/java/tests/PurchaseFlowTest.java
@@ -89,6 +89,7 @@ public class PurchaseFlowTest extends BaseTest{
         inventoryPage.clickCartIcon();
         
         System.out.println("Hii");
+		System.out.println("Byee");
         
         // Verify item is in cart
         Assert.assertEquals(cartPage.getNumberOfItemsInCart(), 1, "Item not added to cart");
